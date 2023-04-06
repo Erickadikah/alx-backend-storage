9c261dea465f8afce114eafaf492c2e70c366e8d
erickadikah <erickadikah2030@gmail.com>
2023-04-07 02:00:55 +0300

    task3 fix

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,8 @@
 -- SQL script that lists all bands from metal_band Glam rock 
 -- as thier man style, ranked by thir--years of being active lifespan in years
 -- attributed formed and split for computing
-SELECT band_name, COALESCE(split, 2023) - formed as lifespan
+
+SELECT band_name, COALESCE(split, 2023) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
