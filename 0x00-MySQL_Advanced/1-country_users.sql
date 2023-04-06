611f4867407ad4506decb2df92e5969bdcb4eab0
erickadikah <erickadikah2030@gmail.com>
2023-04-07 02:06:08 +0300

    fix

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,10 +1,9 @@
 -- SQL scipt that creates following requirements
 -- country, enumeration of countries: US, CO and TN, 
 -- never null (= default will be the first element of the enumeration, here US)
-
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	email VARCHAR(255) NOT NULL UNIQUE,
+	email VARCHAR(255) UNIQUE NOT NULL,
 	name VARCHAR(255),
 	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
