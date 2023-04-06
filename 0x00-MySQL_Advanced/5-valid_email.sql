173d50fe6f5d1cf24444e5d8197763e78595b78e
erickadikah <erickadikah2030@gmail.com>
2023-04-07 01:53:27 +0300

    fix

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,8 +1,6 @@
 -- SQL trigger that resets the attribute valid_email when email has been changed
 -- this function checks if the row being changed refers to the new value
-
 DELIMITER //
-
 CREATE TRIGGER reset_valid_email
 AFTER UPDATE ON users
 FOR EACH ROW
@@ -11,5 +9,5 @@ BEGIN
 	UPDATE users SET valid_email = 0 WHERE id = NEW.email;
 	END IF;
 END//
-
 DELIMITER;
+
