3fd4bceef4fa59f8e1be7f4ce33091259975ae5f
erickadikah <erickadikah2030@gmail.com>
2023-04-07 10:56:59 +0300

    fix typos

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -13,10 +13,10 @@ CREATE PROCEDURE AddBonus
   IN score DECIMAL(10,2)
 )
 BEGIN
-  IF NOT EXISTS(SELECT name FROM project WHERE name = project_name) THEN
-    INSERT INTO project(name) VALUES(project_name);
+  IF NOT EXISTS(SELECT name FROM projects WHERE name = project_name) THEN
+    INSERT INTO projects(name) VALUES(project_name);
   END IF;
-  INSERT INTO corrections(user_id, project_id, score)
-  VALUES(user_id, (SELECT id FROM project WHERE name = project_name), score);
+  INSERT INTO corrections (user_id, project_id, score)
+  VALUES(user_id, (SELECT id FROM projects WHERE name = project_name), score);
 END//
 DELIMITER ;
