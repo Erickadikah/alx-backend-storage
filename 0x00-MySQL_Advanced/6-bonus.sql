b8ad47e57b9f82bcb6d03facd151ca2f6cba8d17
erickadikah <erickadikah2030@gmail.com>
2023-04-07 10:38:12 +0300

    function to create store procedure

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,22 @@
+-- Sql procedure that adds a new orrection foa students
+-- takes three argumemnts @user_id, @project_name, and @score
+-- first we check if a project ith the give name exists in the project
+-- if not it creates a new project with that name
+--  it then inserts a ne corretion into the corrections table with the given user ID
+-- project ID
+
+DELIMITER //
+CREATE PROCEDURE AddBonus
+(
+  IN user_id INT,
+  IN project_name VARCHAR(50),
+  IN score DECIMAL(10,2)
+)
+BEGIN
+  IF NOT EXISTS(SELECT name FROM project WHERE name = project_name) THEN
+    INSERT INTO project(name) VALUES(project_name);
+  END IF;
+  INSERT INTO corrections(user_id, project_id, score)
+  VALUES(user_id, (SELECT id FROM project WHERE name = project_name), score);
+END//
+DELIMITER ;
