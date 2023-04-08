0371c384c27c649c863eef9f8c2466fa6a907e92
erickadikah <erickadikah2030@gmail.com>
2023-04-08 16:53:19 +0300

    listing all bands with Glam lock as their styke

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,17 @@
 -- SQL script that lists all bands from metal_band Glam rock 
--- as thier man style, ranked by thir--years of being active lifespan in years
+-- as thier man style, ranked by their years of being active lifespan in years
 -- attributed formed and split for computing
+-- COALESCE variable holding bands that have not split
+-- assumed their current year is 2022
+
 SELECT band_name,
-COALESCE(split, 2023) - formed AS lifespan
+COALESCE(split, 2022) - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+ORDER BY
+CASE WHEN (COALENSCE(split, 2022) - formed) != 0
+	THEN lifespan
+ELSE 0
+END DESC,
+band_name  DESC;
 
