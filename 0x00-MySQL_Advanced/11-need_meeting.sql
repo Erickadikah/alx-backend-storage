d2898e768524850742979d291e7e6964eb327ed8
erickadikah <erickadikah2030@gmail.com>
2023-04-08 13:16:25 +0300

    filter date an score

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -4,8 +4,7 @@
 -- specifies the laternative condition
 -- checks where for < 80 
 
-CREATE VIEW need_meeting 
-AS SELECT name
+CREATE VIEW need_meetingASSELECT name
 FROM students
-WHERE (score =< 80)
-AND (last_meeting IS NULL OR last_meeting > DATE_SUB(DATE(Now()), INTERVAL 1 MONTH));
+WHERE (score < 80)
+AND (last_meeting IS NULL OR last_meeting < ADDDATE(CURDATE(), INTERVAL -1 MONTH));
--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,3 +1,4 @@
 -- SQL index idx_name_first on table names only the first name is indexed
+-- Firstname is retrieved from the index
 
 CREATE INDEX idx_name_first ON names (name (1));
