54dc9205124d75fa9dcd9aeaa66214ef1b526577
erickadikah <erickadikah2030@gmail.com>
2023-04-08 13:19:42 +0300

    filter date an score

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -4,7 +4,8 @@
 -- specifies the laternative condition
 -- checks where for < 80 
 
-CREATE VIEW need_meetingASSELECT name
+CREATE VIEW need_meeting AS
+SELECT name
 FROM students
 WHERE (score < 80)
 AND (last_meeting IS NULL OR last_meeting < ADDDATE(CURDATE(), INTERVAL -1 MONTH));
