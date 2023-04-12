6c6161d77d5baf915019cc8a36cb4046ee73b4df
erickadikah <erickadikah2030@gmail.com>
2023-04-13 02:48:01 +0300

    cache tracker

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Implementation of an expiring web cache and tracker
+"""
+
+import requests
+import redis
+from functools import wraps
+
+r = redis.Redis()
+
+
+def count_access(method):
+    """
+    Decorator to get count for each access to the page
+    count:{} tracks how many times url was accessed
+    """
+    @wraps(method)
+    def wrapper(url):
+        html_content = method(url)
+        key = "count:{}".format(url)
+        r.incr(key)
+        cached_key = "cached:{}".format(url)
+        cached_response = r.get(cached_key)
+        if cached_response:
+            return cached_response.decode('utf-8')
+        r.setex(cached_key, 10, html_content)
+        return html_content
+    return wrapper
+
+
+@count_access
+def get_page(url: str) -> str:
+    """
+    Returns the response from a request on the url
+    """
+    req = requests.get(url)
+    return req.text
