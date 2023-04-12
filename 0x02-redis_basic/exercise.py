105198157e866cad6ef6631aa39d9bf6b3c104ce
erickadikah <erickadikah2030@gmail.com>
2023-04-12 23:53:15 +0300

    test case reddis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,7 +6,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -23,3 +23,22 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key, fn:Optional[Callable]= None) -> Union[str, bytes,int,
+                                                                    bytes,
+                                                                    int,
+                                                                    float,
+                                                                    None]:
+        if not self._redis.get(key):
+            return None
+        if fn is None:
+            return (self.__redis.get(key))
+
+    def get_str(self, key: str) -> str:
+        """method to retrieve a stored string method"""
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """methotd to retrieve a stored interger volume in storage
+        """
+        return sel.get(key, int)
