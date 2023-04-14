c46f36bcd11ee06d95977db96667fdd15e2b01c0
Erick <erickadikah2030@gmail.com>
2023-04-14 03:56:45 -0700

    log methods

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -16,12 +16,12 @@ if __name__ == '__main__':
     status_check = nginx.count_documents(
         {"$and": [{"method": "GET"}, {"path": "/status"}]})
 
-    print(f"Total number of logs: {log}")
-    print(f"Number of GET requests: {get}")
-    print(f"Number of POST requests: {post}")
-    print(f"Number of PUT requests: {put}")
-    print(f"Number of PATCH requests: {patch}")
-    print(f"Number of DELETE requests: {delete}")
-    print(f"Number of successful GET requests to /status endpoint: {status_check}")
+    print("{} logs\nMethods:\n\
+            method GET: {}\n\
+            method POST:{}\n\
+            method PUT : {}\n\
+            method PATCH: {}\n\
+            method DELETE:{}\n {} status check"
+                    .format(log, get, post, put, patch, delete, status_check))
 
 
